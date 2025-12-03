be0b64671e03fd99d48df9ed3f0e741479dca4e7
dewagedewindraya250506 <dewagedewindraya@gmail.com>
2025-12-03 20:03:05 +0800

    Mengelola file di database

--- inc/config.php
+++ inc/config.php
@@ -1,28 +1,25 @@
 <?php
-// inc/config.php
+// Aktifkan error reporting saat pengembangan
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
-
-// base url (sesuaikan jika menjalankan di subfolder; bila di root http://localhost:8000/ gunakan '')
-define('BASE_URL', ''); // contoh: '/mahasiswa-crud/' jika di subfolder
-
-// Database credentials - sesuaikan sebelum menjalankan
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'crud_mahasiswa');
+// Konfigurasi koneksi database
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', ''); // isi password mysql mu
+define('DB_PASS', '');
+define('DB_NAME', 'mahasiswa_crud');
 
-// upload config
-define('UPLOAD_DIR', __DIR__ . '/../uploads/');
-define('UPLOAD_MAX_SIZE', 2 * 1024 * 1024); // 2 MB
-define('UPLOAD_ALLOWED', ['image/jpeg','image/png','image/jpg']);
-
-// autoload classes from /class
-spl_autoload_register(function ($class_name) {
-    $path = __DIR__ . '/../class/' . $class_name . '.php';
-    if (file_exists($path)) {
-        require_once $path;
+// Autoload class dari folder /class
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . '/../class/' . $class . '.php';
+    if (file_exists($file)) {
+        require_once $file;
     }
 });
+
+// Menu navigasi sederhana
+$NAV_PAGES = [
+    ['title' => 'Beranda',   'url' => 'index.php'],
+    ['title' => 'Mahasiswa', 'url' => 'members.php'],
+    ['title' => 'Tambah',    'url' => 'create.php']
+];
